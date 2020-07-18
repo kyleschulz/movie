9c497bcd684f307dd3c09203d5a83707577f113d
Kyle Schulz <kyle@schulz.com>
2020-07-18 10:58:39 -0500

    updates

--- src/main/java/org/aim/movie/ActorController.java
+++ src/main/java/org/aim/movie/ActorController.java
@@ -1,8 +1,17 @@
 package org.aim.movie;
 
+import java.util.Optional;
+
+import com.jayway.jsonpath.Option;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,6 +25,38 @@ public class ActorController {
     @GetMapping(path = "")
     public @ResponseBody Iterable<Actor> getAllActors() {
         return actorRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public @ResponseBody Actor getActor(@PathVariable(value = "id") Integer id) {
+        Optional<Actor> actor = actorRepository.findById(id);
+        return actor.get();
+    }
+
+    @PostMapping(path = "/")
+    public @ResponseBody String createActor(@RequestBody Actor actor) {
+        actorRepository.save(actor);
+        return "Saved";
+    }
+
+    @PutMapping(path = "/{id}")
+    public @ResponseBody String updateActor(@PathVariable(value = "id") Integer id, @RequestBody Actor actorDetails) {
+
+        Optional<Actor> optionalActor = actorRepository.findById(id);
+        Actor actor = optionalActor.get();
+
+        actor.setFirstName(actorDetails.getFirstName());
+        actor.setLastName(actorDetails.getLastName());
+        actor.setDateOfBirth(actorDetails.getDateOfBirth());
+
+        actorRepository.save(actor);
+        return "Updated";
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public @ResponseBody String deleteActor(@PathVariable(value = "id") Integer id) {
+        actorRepository.deleteById(id);
+        return "Deleted";
     }
 
 }
---
+++ src/main/java/org/aim/movie/Movie.java
@@ -0,0 +1,68 @@
+package org.aim.movie;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "movie_id")
+    private Integer id;
+
+    @Column(name = "movie_name")
+    private String movieTitle;
+
+    private int movieLength;
+
+    private Date releaseDate;
+
+    // Director Id
+
+    // Genre Id
+
+    // Rating Id
+
+    // Actors
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public int getMovieLength() {
+        return movieLength;
+    }
+
+    public void setMovieLength(int movieLength) {
+        this.movieLength = movieLength;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+}
